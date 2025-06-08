197b44cabb46cc4634f6203f93a1105d9c40f2af
valennnttt <151852575+valennnttt@users.noreply.github.com>
2025-06-08 09:22:30 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "crud_app";
+$host = "sql309.infinityfree.com";
+$user = "if0_39182241";
+$pass = "Valenttok2005";
+$db = "if0_39182241_valent";
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
